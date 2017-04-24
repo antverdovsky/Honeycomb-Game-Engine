8fafee793c68ab1ee13f658c0b4b369eca8083ae
Anatoly Tverdovsky <antverdovsky@gmail.com>
2017-04-23 22:17:51 -0500

    Implemented Multi Sample PCF Shadows
    Implemented multiple sample PCF shadow sampling for the Directional
    Light.

--- Honeycomb GE/res/shaders/standard/light/stdBaseLight.glsl
+++ Honeycomb GE/res/shaders/standard/light/stdBaseLight.glsl
@@ -94,12 +94,21 @@ float isInShadow(sampler2D map, vec4 coords, vec3 dir, vec3 norm) {
 	// Get the current depth value of the fragment in relation to the light.
 	float currentDepth = texCoords.z;
 
-	// Sample the shadow map using PCF and return the shadow value
-	return sampleShadowMapPCF(map, texCoords.xy, bias, currentDepth);
+	// Sample the shadow map using PCF and return the shadow value. Multiply
+	// the resulting value by 1 (no change) if the current depth is inside of
+	// the shadow map (this means we correctly calculated the shadow for this
+	// fragment); otherwise multiply the result by 0 (remove the shadow) since
+	// the shadow map did not contain data for this fragment.
+	return sampleShadowMapPCF(map, texCoords.xy, bias, currentDepth) *
+		float(currentDepth <= 1.0F);
 }
 
 float sampleShadowMapPCF(sampler2D map, vec2 coords, float bias, 
 		float curDepth) {
+	// The number of samples used for PCF.
+	const int SAMPLE_COUNT = 1 * 3;	// DO NOT modify the value 3
+	const int SAMPLE_COUNT_SQRD = SAMPLE_COUNT * SAMPLE_COUNT;
+
 	// Get the texel size (inverse of the pixel size of the shadow map)
 	vec2 offset = 1.0F / textureSize(map, 0);
 	
@@ -107,10 +116,10 @@ float sampleShadowMapPCF(sampler2D map, vec2 coords, float bias,
 	float shadow = 0.0F;
 
 	// Loop through all of the surrounding pixels and the current pixel
-	for (int i = 0; i < 9; ++i) {
+	for (int i = 0; i < SAMPLE_COUNT_SQRD; ++i) {
 		// Get the x and y offsets from the current pixel ([-1, 1])
-		int x = i / 3 - 1;
-		int y = i % 3 - 1;
+		int x = i / SAMPLE_COUNT - 1;
+		int y = i % SAMPLE_COUNT - 1;
 
 		// Fetch the depth from the shadow texture map at the current pixel
 		vec2 offsetCoord = coords + vec2(x, y) * offset;
@@ -125,5 +134,5 @@ float sampleShadowMapPCF(sampler2D map, vec2 coords, float bias,
 
 	// Since we sampled 9 pixels in total (8 surrounding + 1 center), we divide
 	// the shadow value by 9 to get the average shadow value.
-	return shadow / 9.0F;
+	return shadow / SAMPLE_COUNT_SQRD;
 }
